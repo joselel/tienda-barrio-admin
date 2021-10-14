d59148ba09a08706024bd9936111eea8f2680d4e
Jose Elel <joselel2011@gmail.com>
2021-10-14 16:27:35 -0600

    cambios-firebase.json

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDXn1uFwYRAiVI5rBX93NpZRXKaZ_M1M2g",
+    authDomain: "tienda-barrio-68bad.firebaseapp.com",
+    projectId: "tienda-barrio-68bad",
+    storageBucket: "tienda-barrio-68bad.appspot.com",
+    messagingSenderId: "844611193076",
+    appId: "1:844611193076:web:7785fb7d54363b61eb76a1",
+    measurementId: "G-023QZ5YGHP"
+}
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyDXn1uFwYRAiVI5rBX93NpZRXKaZ_M1M2g",
+    authDomain: "tienda-barrio-68bad.firebaseapp.com",
+    projectId: "tienda-barrio-68bad",
+    storageBucket: "tienda-barrio-68bad.appspot.com",
+    messagingSenderId: "844611193076",
+    appId: "1:844611193076:web:7785fb7d54363b61eb76a1",
+    measurementId: "G-023QZ5YGHP"
+}
 };
 
 /*
