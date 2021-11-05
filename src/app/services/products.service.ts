083fe4193fe8774d4e10bfbba6cd57c57563a91b
Jose Elel <joselel2011@gmail.com>
2021-11-04 21:29:39 -0600

    cambios-productos

---
+++ src/app/services/products.service.ts
@@ -0,0 +1,40 @@
+import { EventEmitter, Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import firebase from "firebase/compat/app";
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductsService {
+  productos:EventEmitter<number> = new EventEmitter();
+  db = firebase.firestore();
+  constructor(
+    private firestore: AngularFirestore) {
+
+     }
+
+  getProducts(): Observable<any> {
+    return this.firestore.collection('products', ref => ref.orderBy('name', 'desc')).snapshotChanges();
+  }
+
+  getOrder(id: string): Observable<any> {
+    return this.firestore.collection('orders').doc(id).snapshotChanges();
+  }
+
+  getOrdersPending(): Observable<any> {
+    return this.firestore.collection('orders', ref => ref.orderBy('FechaInicio', 'desc').where('estado','==','Recibido')).snapshotChanges();
+  }
+
+  getOrdersCancel(): Observable<any> {
+    return this.firestore.collection('orders', ref => ref.orderBy('FechaInicio', 'desc').where('estado','==','Cancelado')).snapshotChanges();
+  }
+
+  getOrdersPrepared(): Observable<any> {
+    return this.firestore.collection('orders', ref => ref.orderBy('FechaInicio', 'desc').where('estado','==','Preparado')).snapshotChanges();
+  }
+
+  UpdateStateOrder(id:any, estado:string){
+    return firebase.firestore().collection("orders").doc(id).update({estado: estado});
+  }
+}
